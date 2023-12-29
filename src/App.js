3f9ffa478c468c97900d39d8feb868b4b27674b9
Alex7D3 <ao11@my.yorku.ca>
2023-12-29 16:15:00 -0500

    working intersection observer with nav

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-import Title from "./components/Title";
+import Scene from "./components/Scene";
 import Nav from "./components/Nav";
-import About from "./components/About";
-import Projects from "./components/Projects";
+
+import "./styles/home.css";
+import "./styles/about.css";
+import "./styles/projects.css"
+
 import React, { useRef, useState, useEffect } from "react";
 
 function App() {
-    const titleRef = useRef(null);
+    const homeRef = useRef(null);
     const aboutRef = useRef(null);
     const projectsRef = useRef(null);
 
-    const [intersecting, setIntersecting] = useState("Title");
+    const [intersecting, setIntersecting] = useState("title");
     
     useEffect(() => {
         const observer = new IntersectionObserver(entries => {
             entries.forEach(entry => {
                 if(entry.isIntersecting)
-                    setIntersecting(entry.target.nodeName);
+                    setIntersecting(entry.target.id);
+                if(entry.isIntersecting)
+                    console.log(entry.target.id)
             });
-        }, { rootMargin: "-300px" });
+        }, { rootMargin: "-50%" });
 
-        // [titleRef, aboutRef, projectsRef].forEach((elem) => {
-        //     observer.observe(elem.current);
-        // });
+        [homeRef, aboutRef, projectsRef].forEach((elem) => {
+            observer.observe(elem.current);
+        });
         
         return () => observer.disconnect();
     }, []);
@@ -31,9 +36,37 @@ function App() {
     return (
         <div className="App">
             <Nav intersecting={intersecting}/>
-            <Title  animation={true} />
-            <About animation={true} />
-            <Projects animation={true} />
+            <section id="home" ref={homeRef}>
+                <header><h1>Alex Odorico</h1></header>
+                <Scene />
+            </section>
+            <section id="about" ref={aboutRef}>
+            <h1>About</h1>
+            <article className="desc">
+                I am a Computer Science major in my 3rd year of studies at York University.
+                <p>Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem
+                    Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum .</p></article>
+            </section>
+            <section id="projects" ref={projectsRef}>
+                <h1>Projects</h1>
+                <div>
+                    <h2>Sudoku Prime</h2>
+                    <p>efefefefefefef</p>
+                    <p>fefefef</p>
+                </div>
+
+                <div>
+                    <h2>MyAniBot</h2>
+                    <p>efefef</p>
+                    <p>fefefef</p>
+                </div>
+
+                <div>
+                    <h2>Project3</h2>
+                    <p>fefefef</p>
+                    <p>fefefef</p>
+                </div>
+            </section>
         </div>
     );
 }
