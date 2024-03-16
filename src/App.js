dad1ecd5848d23db71accb67a82b29462c580f75
Alex7D3 <ao11@my.yorku.ca>
2024-03-15 21:14:10 -0400

    style updates and links

--- src/App.js
+++ src/App.js
@@ -21,8 +21,6 @@ function App() {
             entries.forEach(entry => {
                 if(entry.isIntersecting)
                     setIntersecting(entry.target.id);
-                if(entry.isIntersecting)
-                    console.log(entry.target.id)
             });
         }, { rootMargin: "-50%" });
 
@@ -42,25 +40,25 @@ function App() {
                     <header><h1><div id="caret">~$&nbsp;</div><div id="text">Alex Odorico</div><div id="cursor"/>&nbsp;&nbsp;</h1></header>
                 </section>
                 <section id="about" ref={aboutRef}>
-                    <div>
+                    <div className="content">
                         <h2>About</h2>
+                        <img id="me" alt="myself" src="\me.jpg"></img>
                         <article className="desc">
                         <p>I am a Computer Science major in my 3rd year of studies at York University.
-                        Pleased to meet you!</p>
+                        Pleased to meet you.
+                        </p>
         
                         <p>
-                        Technology has always been a part of my life. As a child I always wondered what the video games
-                        I enjoyed look like on the inside, or how computers could talk to each other continents apart.
-                        This curiosity led me to learning coding in highschool, and from that point on I knew the career 
-                        I wanted to pursue.
-                        <p>I have a particular fondness for
-                        Data Structures and Algorithms. I am actively looking for opportunities to expand my skills.</p>
-                        </p>
+                        Technology has always been a part of my life. As a child, I wondered how the video games I enjoyed looked on the inside
+                        or how computers could talk to each other continents apart.
+                        This curiosity led me to learn programming in high school, and from that point, I knew the career I wanted to pursue.</p>
+                        <p>I am currently looking for opportunities to expand my skills.</p>
+                        
                         </article>
                     </div>
-                    <div>
+                    <div className="content">
                         <h2>Tools and Technologies</h2>
-                        <div>
+                        <div className="icon-grid">
                             {icons.map(({ path, alt }, idx) => 
                                 <img key={idx} src={path} alt={alt} title={alt} width="60px" height="60px" />
                             )}
@@ -68,10 +66,18 @@ function App() {
                     </div>
                 </section>
                 <section id="projects" ref={projectsRef}>
-                    <div>
-                        <h2>Projects</h2>
+                    <h2>Projects</h2>
+                    <div className="project-grid">
                         {projects.map(({ name, desc, tools, url }, idx) => 
-                            <div key={idx}><h3>{name}</h3></div>
+                            <div key={idx} className="content">
+                                <h3>{name} {url && <a className="github" href={url} target="_blank" rel="noreferrer" >github</a>}</h3>
+                                <article>{desc}</article>
+                                <div className="icon-grid">
+                                    {tools.map(({ path, alt }, idx) => 
+                                        <img key={idx} src={path} alt={alt} title={alt} width="60px" height="60px" />
+                                    )}
+                                </div>
+                            </div>
                         )}
                     </div>
                 </section>
